83845ffeef39e71815b0e91e5b4f20ebf32f604c
Chihiro88G <1114chihiro@gmail.com>
2025-04-09 20:11:13 -0400

    composition based approach

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,44 @@
 import { MatchReader } from "./MatchReader";
+import { CsvFileReader } from "./CsvFileReader";
+import { MatchResult } from "./MatchResult";
 
-const reader = new MatchReader('football.csv');
-reader.read();
+import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
-let manUnitedWins = 0;
 
-for (let match of reader.data) {
-  if (match[1] === 'Man United' && match[5] === 'H') {
-    manUnitedWins++;
-  } else if (match[2] === 'Man United' && match[5] === 'A') {
-    manUnitedWins++;
-  }
-}
+// INHERITANCE APPROACH
+// const reader = new MatchReader('football.csv');
+// reader.read();
 
-console.log(`Man United won ${manUnitedWins} times`);
+// let manUnitedWins = 0;
+
+// for (let match of reader.data) {
+//   if (match[1] === 'Man United' && match[5] === 'H') {
+//     manUnitedWins++;
+//   } else if (match[2] === 'Man United' && match[5] === 'A') {
+//     manUnitedWins++;
+//   }
+// }
+
+// console.log(`Man United won ${manUnitedWins} times`);
+
+
+
+// INTERFACE (COMPOSITION) APPROACH
+// create object that satisfies the "DataReader" interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// create instance of MatchReader
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
+
+const summary = new Summary(
+  new WinsAnalysis('Man United'),
+  // new ConsoleReport()
+  new HtmlReport()
+);
+
+summary.buildAndPrintReport(matchReader.matches);
+
